935d360971c6a67528189e2c267d53a85ac992d2
michal-at-git <phantom-black@myopera.com>
2013-01-20 16:55:56 +0100

    Extended Euclidean algorithm

--- gcd.h
+++ gcd.h
@@ -22,4 +22,54 @@ int gcd(int a, int b)
  std::cout << result << "\n";
  return result;
 }
-#endif
+
+//Extended Euclideam algorithm
+
+void egcd(int a, int b) {
+  int xa = 1;
+  int ya = 0;
+  int xb = 0;
+  int yb = 1;
+  int c = 0;
+  int x;
+  int y;
+  std::cout << "+______________________________________________+\n";
+  std::cout << "| a=" << a << " | b=" << b << " | c=" << c <<" | xa=" << xa << " | ya=" << ya <<" | xb=" << xb << " | yb=" << yb << " |\n";
+  std::cout << "+______________________________________________+\n";
+
+
+  while ((a*b) > 0)
+  {
+      if (a>=b)
+      {
+	c = a/b;
+	a = a%b;
+	xa = xa - xb * c;
+	ya = ya - yb * c;
+	std::cout << "|a=" << a << " | b=" << b << " | c=" << c << " | xa=" << xa << " | ya=" << ya <<" | xb=" << xb << " | yb=" << yb << " |\n";
+      }
+      else
+      {
+	c = b/a;
+	b = b%a;
+	xb = xb - xa * c;
+	yb = yb - ya * c;
+		std::cout << "| a=" << a << " | b=" << b << " | c=" << c << " | xa=" << xa << " | ya=" << ya <<" | xb=" << xb << " | yb=" << yb << " |\n";
+
+      }
+      std::cout << "+______________________________________________+\n";
+      
+      if (a>0) {
+	x = xa;
+	y = ya;
+      }
+      else
+      {
+	x = xb;
+	y = yb;
+      }
+	std::cout << "x = " << x << " y = " << y << "\n";
+
+  }
+}
+#endif 
